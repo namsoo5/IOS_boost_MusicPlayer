fe8ebe4111ab30f82b68d232e89138c5ad41f8c8
namsoo <are34@naver.com>
2019-02-04 15:43:55 +0900

    뮤직플레이어 기본레이아웃구성

---
+++ IOS_boost_MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_boost_MusicPlayer
+//
+//  Created by 남수김 on 04/02/2019.
+//  Copyright © 2019 남수김. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
